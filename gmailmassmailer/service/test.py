77a2127212b9ac50c49756cf0e27fd9d1add79bb
duncan <duncan@localhost.localdomain>
2018-02-19 17:15:02 +0300

    initial commit

---
+++ gmailmassmailer/service/test.py
@@ -0,0 +1,7 @@
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+p2=os.path.dirname(os.path.abspath(__file__))
+p3=os.path.abspath(__file__)
+print parentdir
+print p2
+print p3
